603cd85343937de332158c38fdc282f4fa39874e
Tarek <tarekakkawi89@gmail.com>
2017-10-29 18:20:17 +0000

    New branch

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,50 +26,78 @@ namespace WPFCalculator
         }
 
 
+        
+        int calculation ;
+        string value1;
+        string value2;
+        int number2;
+        int number1;
+        bool btnAdd_Clicked, btnSub_Clicked, btnMult_Clicked, btnDiv_Clicked;
+
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            int value1, value2, calculation;
+            value1 = txtvalue1.Text;
 
-            value1 = Convert.ToInt32(txtvalue1.Text);
-            value2 = Convert.ToInt32(txtvalue2.Text);
-            calculation = value1 + value2;
-            txtcalculation.Text = calculation.ToString();
+            number1 = Convert.ToInt32(value1);
+
+            btnAdd_Clicked = true;
         }
+
 
         private void btnSub_Click(object sender, RoutedEventArgs e)
         {
-            int value1, value2, calculation;
+            value1 = txtvalue1.Text;
 
-            value1 = Convert.ToInt32(txtvalue1.Text);
-            value2 = Convert.ToInt32(txtvalue2.Text);
-            calculation = value1 - value2;
-            txtcalculation.Text = calculation.ToString();
+            number1= Convert.ToInt32(txtvalue1.Text);
+
+            btnSub_Clicked = true;
+  
         }
 
         private void btnMult_Click(object sender, RoutedEventArgs e)
         {
-            int value1, value2, calculation;
+            value1 = txtvalue1.Text;
 
-            value1 = Convert.ToInt32(txtvalue1.Text);
-            value2 = Convert.ToInt32(txtvalue2.Text);
-            calculation = value1 * value2;
-            txtcalculation.Text = calculation.ToString();
+            number1 = Convert.ToInt32(txtvalue1.Text);
 
-
+            btnMult_Clicked = true;
         }
 
         private void btnDiv_Click(object sender, RoutedEventArgs e)
         {
-            int value1, value2, calculation;
+            value1 = txtvalue1.Text;
 
-            value1 = Convert.ToInt32(txtvalue1.Text);
-            value2 = Convert.ToInt32(txtvalue2.Text);
-            calculation = value1 / value2;
-            txtcalculation.Text = calculation.ToString();
+            number1 = Convert.ToInt32(txtvalue1.Text);
 
-
+            btnDiv_Clicked = true;
         }
 
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            value2 = txtvalue2.Text;
+            number2 = Convert.ToInt32(value2);
 
+            if (btnAdd_Clicked == true)
+            {
+                calculation = number1 + number2;
+                txtcalculation.Text = calculation.ToString();
+            }
+            else if (btnSub_Clicked == true)
+            {
+                calculation = number1 - number2;
+                txtcalculation.Text = calculation.ToString();
+            }
+            else if (btnMult_Clicked == true)
+            {
+                calculation = number1 * number2;
+                txtcalculation.Text = calculation.ToString();
+            }
+            else if (btnDiv_Clicked == true)
+            {
+                calculation = number1 / number2;
+                txtcalculation.Text = calculation.ToString();
+            }
+
+        }
     }
 }
